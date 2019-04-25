b0dbd8536f7c1105bcfc7dcf6d8139c1e819bde1
Nguyen Anh Tan <anhtannguyen1999@gmail.com>
2019-04-25 22:45:18 +0700

    MapEditer v2.0
    Map editer 2.0

---
+++ 1_MapEditer/CatMap/CatMap/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace CatMap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CatMap = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxWidth = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxHeight = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnMix = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CatMap
+            // 
+            this.CatMap.Location = new System.Drawing.Point(168, 29);
+            this.CatMap.Name = "CatMap";
+            this.CatMap.Size = new System.Drawing.Size(88, 60);
+            this.CatMap.TabIndex = 0;
+            this.CatMap.Text = "Cut";
+            this.CatMap.UseVisualStyleBackColor = true;
+            this.CatMap.Click += new System.EventHandler(this.CatMap_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Input: \"InOut\\map.png\"";
+            // 
+            // tbxWidth
+            // 
+            this.tbxWidth.Location = new System.Drawing.Point(72, 29);
+            this.tbxWidth.Name = "tbxWidth";
+            this.tbxWidth.Size = new System.Drawing.Size(79, 22);
+            this.tbxWidth.TabIndex = 3;
+            this.tbxWidth.Text = "16";
+            this.tbxWidth.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbxWidth_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Size to cut:";
+            // 
+            // tbxHeight
+            // 
+            this.tbxHeight.Location = new System.Drawing.Point(72, 67);
+            this.tbxHeight.Name = "tbxHeight";
+            this.tbxHeight.Size = new System.Drawing.Size(79, 22);
+            this.tbxHeight.TabIndex = 5;
+            this.tbxHeight.Text = "16";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Width";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Height";
+            // 
+            // btnMix
+            // 
+            this.btnMix.Location = new System.Drawing.Point(305, 32);
+            this.btnMix.Name = "btnMix";
+            this.btnMix.Size = new System.Drawing.Size(225, 55);
+            this.btnMix.TabIndex = 8;
+            this.btnMix.Text = "Mix";
+            this.btnMix.UseVisualStyleBackColor = true;
+            this.btnMix.Click += new System.EventHandler(this.btnMix_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(302, 92);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(162, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Input: \"InOut\\tileSet.png\"";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(302, 109);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(150, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Input: \"InOut\\matrix.txt\"";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 126);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(162, 17);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Output: \"InOut\\matrix.txt\"";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 109);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(174, 17);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Output: \"InOut\\tileSet.png\"";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(302, 126);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(241, 17);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Output: \"InOut\\gameMap_Mixed.png\"";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 157);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnMix);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbxHeight);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbxWidth);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CatMap);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CatMap;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxWidth;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxHeight;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnMix;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ 1_MapEditer/CatMap/CatMap/Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CatMap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int width = 16, height = 16;
+
+        private void CatMap_Click(object sender, EventArgs e)
+        {
+            width = 16; height = 16;
+            int imgWidth, imgHeight;
+            string matrixLine = "";
+            Int32.TryParse(tbxWidth.Text,out width);
+            Int32.TryParse(tbxHeight.Text, out height);
+            
+            
+            //Load image
+            Image img = Image.FromFile(@"InOut\map.png");
+            if (img == null)
+                MessageBox.Show(@"Missing Inout\map.png");
+            imgWidth = img.Width;
+            imgHeight = img.Height;
+
+            if(File.Exists(@"InOut\matrix.txt"))
+            {
+                File.Delete(@"InOut\matrix.txt");
+            }
+            if (File.Exists(@"InOut\matrix_tmp.txt"))
+            {
+                File.Delete(@"InOut\matrix_tmp.txt");
+            }
+
+            //Create list
+            List<Bitmap> lstTile = new List<Bitmap>();
+            Bitmap cloned;
+            Bitmap tileSet=null;
+            Rectangle rect;
+            //Crop image
+            for (int y=0;y<imgHeight;y+=height)
+            {
+                matrixLine = "";
+                for(int x=0;x<imgWidth;x+=width)
+                {
+                    rect = new Rectangle(x, y, width, height);
+                    cloned = new Bitmap(img).Clone(rect, img.PixelFormat);
+                    bool isExited = false;
+                    for (int i = 0; i < lstTile.Count; i++)
+                    {
+                        //pictureBox1.Image = cloned;
+                        //pictureBox2.Image = lstTile[i];
+                        
+                        //So sanh xem cái hình vừa clone có tồn tại chưa
+                        if(Equals(cloned,lstTile[i]))//Nếu có rồi thì...
+                        {
+                            isExited = true;
+                            //matrixLine += (i) + " ";
+                            matrixLine += (i+1) + " "; // Viết i+1 vô ma trận map
+                            //(chú ý là ma trận bắt đầu từ 1 chứ k phải từ 0 như list) do số 0 mình chừa cho mục đích k có gì
+                        }
+
+                    }
+                    if(!isExited)//Nếu cái hình vừa so sánh chưa có thì...
+                    {
+                        lstTile.Add(cloned);//Thêm nó vào list
+                        //matrixLine +=lstTile.Count-1+ " ";
+                        matrixLine += lstTile.Count + " ";//Viết số của nó vô ma trận 
+                        //add vo bitmap
+                        tileSet =AppendBitmap(tileSet, cloned, 0);
+                    }
+                    //MessageBox.Show("a");
+                    GC.Collect();
+                }
+                WriteLineToFileWithUTF8(@"InOut\matrix_tmp.txt", matrixLine);
+                
+            }
+            //First line
+            string line;
+            WriteLineToFileWithUTF8(@"InOut\matrix.txt", lstTile.Count.ToString()+" "+imgHeight/height+" "+imgWidth/width);
+            System.IO.StreamReader file = new System.IO.StreamReader(@"InOut\matrix_tmp.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                WriteLineToFileWithUTF8(@"InOut\matrix.txt", line);
+            }
+            file.Close();
+
+            if (File.Exists(@"InOut\matrix_tmp.txt"))
+            {
+                File.Delete(@"InOut\matrix_tmp.txt");
+            }
+            
+            MessageBox.Show("Done!");
+
+            //Save tileSet
+            if (File.Exists(@"InOut\tileSet.png"))
+            {
+                File.Delete(@"InOut\tileSet.png");
+            }
+            tileSet.Save(@"InOut\tileSet.png", System.Drawing.Imaging.ImageFormat.Png);
+
+
+        }
+
+        #region Mix
+        private void btnMix_Click(object sender, EventArgs e)
+        {
+            //Khoi tao bmp luu map
+            Bitmap tile;
+            Bitmap gameMap=null;
+            Rectangle rect;
+
+            width = 16; height = 16;
+            string line = "";
+            
+            //Lay w h cua 1 tile 
+            Int32.TryParse(tbxWidth.Text, out width);
+            Int32.TryParse(tbxHeight.Text, out height);
+
+            //Load tile map image
+            Image img = Image.FromFile(@"InOut\tileSet.png");
+            if (img == null)
+            {
+                MessageBox.Show(@"Missing Inout\tileSet.png");
+                return;
+            }
+              
+            //Load matrix
+            if (!File.Exists(@"InOut\matrix.txt"))
+            {
+                MessageBox.Show(@"Missing Inout\matrix");
+                return;
+            }
+            System.IO.StreamReader file = new System.IO.StreamReader(@"InOut\matrix.txt");
+            line = file.ReadLine();//First line
+
+            string[] row;
+            int lineCount = 0;
+            while ((line = file.ReadLine()) != null)
+            {
+                //Xu ly
+                //Cat line ra ma tran dong
+                row=line.Split(' ');//dư 1 phần
+                for(int i=0;i<row.Count()-1;i++)
+                {
+                    //Lay so thu tu img
+                    int numPieceOfMap = 0;
+                    Int32.TryParse(row[i], out numPieceOfMap);
+                    if(numPieceOfMap!=0)
+                    {
+                        //Lay tile
+                        rect = new Rectangle((numPieceOfMap - 1) * width, 0, width, height);
+                        tile = new Bitmap(img).Clone(rect, img.PixelFormat);
+                        //Them vao map
+                        gameMap = AppendBitmapAt(gameMap, tile, i * width, lineCount * height);
+
+                        GC.Collect();
+                    }
+                }
+                lineCount++;
+                
+            }
+            file.Close();
+            //Save file
+            gameMap.Save(@"InOut\gameMap_Mixed.png", System.Drawing.Imaging.ImageFormat.Png);
+            MessageBox.Show("Done!");
+
+        }
+
+        #endregion
+
+        #region Function
+
+        public Bitmap AppendBitmap(Bitmap source, Bitmap target, int spacing)
+        {
+            if (source != null && target != null)
+            {
+                int w = source.Width + target.Width + spacing;
+                int h = Math.Max(source.Height, target.Height);
+                
+                Bitmap bmp = new Bitmap(w, h);
+
+                using (Graphics g = Graphics.FromImage(bmp))
+                {
+                    g.DrawImage(source, 0, 0); //Draw image from a point
+                    g.DrawImage(target,  source.Width + spacing,0);//Draw image from a point
+                }
+
+                return bmp;
+            }
+            else if (source == null)
+            {
+                return target;
+            }
+            else
+                return source;
+        }
+        //
+        public Bitmap AppendBitmapAt(Bitmap source, Bitmap target, int xPos,int yPos)
+        {
+            if (source != null && target != null)
+            {
+                int w, h;
+                if (xPos + target.Width > source.Width)
+                    w = xPos + target.Width;
+                else
+                    w = source.Width;
+                if (yPos + target.Height > source.Height)
+                    h = yPos + target.Height;
+                else
+                    h = source.Height;
+                Bitmap bmp = new Bitmap(w, h);
+
+                using (Graphics g = Graphics.FromImage(bmp))
+                {
+                    g.DrawImage(source, 0, 0); //Draw image from a point
+                    g.DrawImage(target, xPos,yPos);//Draw image from a point
+                }
+
+                return bmp;
+            }
+            else if (source == null)
+            {
+                return target;
+            }
+            else
+                return source;
+        }
+        private bool Equals(Bitmap bmp1, Bitmap bmp2)
+        {
+            
+            if (!bmp1.Size.Equals(bmp2.Size))
+            {
+                return false;
+            }
+            for (int x = 0; x < bmp1.Width; ++x)
+            {
+                for (int y = 0; y < bmp1.Height; ++y)
+                {
+                    if (bmp1.GetPixel(x, y) != bmp2.GetPixel(x, y))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+            
+        }
+        private void WriteLineToFileWithUTF8(string filePath, string txt)
+        {
+            using (FileStream fs = new FileStream(filePath, FileMode.Append))
+            {
+                using (StreamWriter writer = new StreamWriter(fs, Encoding.UTF8))
+                {
+                    writer.WriteLine(txt);
+                }
+            }
+        }
+        private void tbxWidth_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+    #endregion
+}
